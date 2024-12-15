b90da876b517bdfcccc4bbdf0dde5c4216c1af17
Trickez <71783079+Trickez@users.noreply.github.com>
2024-12-15 17:58:52 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var app = express()
 
 
 
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-});
-
-
-
-;app.listen(3000, () => {
+app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
+
+
+app.get("/", (req, res, next) => {
+    console.log("ttttt")
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
